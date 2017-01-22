043e79f522b2359e20e4e274c16eaeb644a57c8c
Jon Groth <tripl@DESKTOP-0HIPL2H>
2017-01-21 19:26:01 -0600

    Initial Commit

---
+++ jgroth1a1/src/jgroth1a1/Mainform.java
@@ -0,0 +1,73 @@
+package jgroth1a1;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Mainform extends JFrame {
+
+	private JPanel contentPane;
+	private JLabel grossPayLabel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Mainform frame = new Mainform();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Mainform() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Gross Pay:");
+		lblNewLabel.setBounds(10, 41, 71, 14);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Calculate");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				do_btnNewButton_actionPerformed(arg0);
+			}
+		});
+		btnNewButton.setBounds(143, 84, 100, 23);
+		contentPane.add(btnNewButton);
+		
+		grossPayLabel = new JLabel("");
+		grossPayLabel.setBounds(91, 41, 215, 14);
+		contentPane.add(grossPayLabel);
+	}
+
+	protected void do_btnNewButton_actionPerformed(ActionEvent arg0) {
+		int hours = 40;
+		double grossPay, payRate = 25.0;
+		
+		grossPay = hours * payRate;
+		
+		grossPayLabel.setText("Your gross pay is $" + grossPay);
+				
+	}
+}
